0cec4c3438d873e51603609937a79775ac1bbcfb
Zoey Child <imperialoctopus@gmail.com>
2020-03-05 23:19:30 +0000

    Add styles

---
+++ lib/screens/landscape_book_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LandscapeBookScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _LandscapeBookScreenState();
+}
+
+class _LandscapeBookScreenState extends State<LandscapeBookScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
