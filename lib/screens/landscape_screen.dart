b1d409ad7a521f3b18f002d8ce664d7c0f61dfef
Zoey Child <imperialoctopus@gmail.com>
2020-03-05 23:09:30 +0000

    Add landscape screen

---
+++ lib/screens/landscape_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LandscapeScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _LandscapeScreenState();
+}
+
+class _LandscapeScreenState extends State<LandscapeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
