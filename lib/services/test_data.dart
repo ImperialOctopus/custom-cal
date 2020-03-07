d48b1a493aa7949ef6130e5b7fa1a6397d91825f
Zoey Child <imperialoctopus@gmail.com>
2020-03-07 22:03:06 +0000

    Add more images to test data

--- lib/services/test_data.dart
+++ lib/services/test_data.dart
@@ -30,28 +30,25 @@ Book testBook = Book(
           name: 'First Page',
           content: <Widget>[
             Text('First li2ne'),
-            Text('Second li2ne'),
-            Text('Thi2rd line'),
             Center(
               child: Image(
                 image: NetworkImage(
-                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-                height: 300,
+                    'https://images.unsplash.com/photo-1583582941679-75e0d4e76f8e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'),
+                height: 500,
               ),
             ),
+            Text('LEAVES'),
+            Text('LEAFES'),
           ],
         ),
         Page(
-          name: 'Firs3t Page',
+          name: 'Page with a Lady',
           content: <Widget>[
-            Text('First line'),
-            Text('Second line'),
-            Text('Third line'),
             Center(
               child: Image(
                 image: NetworkImage(
-                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-                height: 300,
+                    'https://images.unsplash.com/photo-1583599377295-f1fc27794f30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'),
+                height: 600,
               ),
             ),
           ],
@@ -59,9 +56,15 @@ Book testBook = Book(
         Page(
           name: 'First 4',
           content: <Widget>[
-            Text('First line'),
-            Text('Second line'),
-            Text('Third line'),
+            Text('Oooooo'),
+            Text('Zooom'),
+            Text('Outrageous'),
+          ],
+        ),
+        Page(
+          name: 'Owl Again',
+          content: <Widget>[
+            Text('Omaewamou Shindeiru'),
             Center(
               child: Image(
                 image: NetworkImage(
@@ -71,17 +74,65 @@ Book testBook = Book(
             ),
           ],
         ),
+      ],
+    ),
+    Section(
+      name: 'Second Section',
+      label: Text('2'),
+      pages: <Page>[
         Page(
-          name: '5 Page',
+          name: 'First Page',
           content: <Widget>[
             Text('First line'),
             Text('Second line'),
             Text('Third line'),
+          ],
+        ),
+        Page(
+          name: 'Second Page',
+          content: <Widget>[
+            Text('First li2ne'),
+            Text('Second li2ne'),
+            Text('Thi2rd line'),
+            Center(
+              child: Image(
+                image: NetworkImage(
+                    'https://images.unsplash.com/photo-1583593185947-363fb444b930?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=701&q=80'),
+                height: 300,
+              ),
+            ),
+          ],
+        ),
+        Page(
+          name: 'Last Page',
+          content: <Widget>[
+            Text('Thank you'),
+            Text('For playing'),
+            Text(
+                'Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'),
             Center(
               child: Image(
                 image: NetworkImage(
                     'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
                 height: 300,
+              ),
+            ),
+          ],
+        ),
+      ],
+    ),
+    Section(
+      name: 'Second Section',
+      label: Icon(Icons.pool),
+      pages: <Page>[
+        Page(
+          name: 'Final Section',
+          content: <Widget>[
+            Center(
+              child: Image(
+                image: NetworkImage(
+                    'https://images.unsplash.com/photo-1583444012262-00185bf33cc6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'),
+                height: 700,
               ),
             ),
           ],
