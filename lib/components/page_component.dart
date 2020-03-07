6398fa12c683aa0844f74233b8c45d827ab94925
Zoey Child <imperialoctopus@gmail.com>
2020-03-07 19:04:53 +0000

    Lighten page fold colour

--- lib/components/page_component.dart
+++ lib/components/page_component.dart
@@ -6,7 +6,6 @@ enum FoldEdge { left, right, none }
 class PageComponent extends StatelessWidget {
   final double padding = 16;
   final FoldEdge foldEdge;
-
   final Page page;
 
   const PageComponent({
@@ -44,7 +43,7 @@ class PageComponent extends StatelessWidget {
       case FoldEdge.right:
         return BoxDecoration(
           gradient: LinearGradient(
-              colors: [const Color(0x11666666), const Color(0x00000000)],
+              colors: [const Color(0x10666666), const Color(0x00000000)],
               begin: Alignment.centerRight,
               end: Alignment(0.8, 0.0),
               tileMode: TileMode.clamp),
@@ -53,7 +52,7 @@ class PageComponent extends StatelessWidget {
       case FoldEdge.left:
         return BoxDecoration(
           gradient: LinearGradient(
-              colors: [const Color(0x11666666), const Color(0x00000000)],
+              colors: [const Color(0x10666666), const Color(0x00000000)],
               begin: Alignment.centerLeft,
               end: Alignment(-0.8, 0.0),
               tileMode: TileMode.clamp),
