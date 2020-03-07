c964d9ae7617f9fff8acb606c54fa9a7361883e6
Zoey Child <imperialoctopus@gmail.com>
2020-03-07 13:04:27 +0000

    Add visual parameters to section select

--- lib/components/section_select_component.dart
+++ lib/components/section_select_component.dart
@@ -1,30 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:prototype_cal/components/section_select_button_component.dart';
+import 'package:prototype_cal/model/section.dart';
 
 class SectionSelectComponent extends StatelessWidget {
-  final List<Widget> labels;
+  final List<Section> sections;
   final int activeSection;
-  final Function(int) onSectionChanged;
+  final Function(int) onSectionTapped;
+  final double sectionTabHeight;
+  final double sectionTabWidth;
+  final double sectionTabSpacing;
 
   const SectionSelectComponent({
     Key key,
-    @required this.labels,
+    @required this.sections,
     @required this.activeSection,
-    @required this.onSectionChanged,
+    @required this.onSectionTapped,
+    this.sectionTabHeight,
+    this.sectionTabWidth,
+    this.sectionTabSpacing = 0,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: List<Widget>.generate(
-        labels.length,
-        (int i) => SectionSelectButtonComponent(
-          label: labels[i],
-          index: i,
-          active: i == activeSection,
-          onTap: (int i) => onSectionChanged(i),
-        ),
-      ),
-    );
+    return Row(children: buildRow(sections));
+  }
+
+  List<Widget> buildRow(List<Section> sections) {
+    var list = <Widget>[];
+    for (int i = 0; i < sections.length; i++) {
+      list.add(SectionSelectButtonComponent(
+        label: sections[i].label,
+        index: i,
+        active: i == activeSection,
+        height: sectionTabHeight,
+        width: sectionTabWidth,
+        onTap: (int i) => onSectionTapped(i),
+      ));
+      if (sectionTabSpacing > 0) {
+        list.add(SizedBox(width: sectionTabSpacing));
+      }
+    }
+    return list;
   }
 }
