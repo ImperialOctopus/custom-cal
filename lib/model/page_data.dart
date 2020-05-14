9d5f99197e8d53911276f823dc131e563c3b49b2
Zoey Child <imperialoctopus@gmail.com>
2020-05-14 18:39:14 +0100

    Remove note parameter from pagedata

--- lib/model/page_data.dart
+++ lib/model/page_data.dart
@@ -3,11 +3,9 @@ import 'package:flutter/material.dart';
 class PageData {
   final String name;
   final List<Widget> content;
-  final String note;
 
   const PageData({
     @required this.name,
     @required this.content,
-    this.note,
   });
 }
